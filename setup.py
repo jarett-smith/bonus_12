59b8f921165c877888e22b2c2ec8fe254852b10b
jarett-smith <jsmit560@vols.utk.edu>
2023-11-29 21:08:13 -0500

    adding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='my_pkg',
+      description='Adding and Subtracting',
+      author='Jarett Smith',
+      url='https://github.com/jarett-smith/bonus_12',
+      packages=['my_pkg'],
+      license='MIT',
+      )
